71920d0160d23cdc9086a92fae2772093546a6c7
daddies20 <daddies20@yahoo.com>
2016-08-20 15:07:13 +0200

    Update main.js

--- firstgame/main.js
+++ firstgame/main.js
@@ -2,32 +2,20 @@ var mainState = {
 	
 	preload: function() {
 		game.load.imagine("player","assets/player.png");
-
 	},
 create: function() 
   game.state.backgroundColor = '#3498db';
-
-  game.physics.startSystem(phaser.physics.ARCADE);
-
-  game.renderer.rendererSession.roundpixels = true;
+  game.physics.startSystem(phaser.Physics.ARCADE);
+  game.renderer.rendererSession.roundPixels = true;
   var player = game.add.sprite(game.width/250, 170, 'player');
-  this.player = game.add.spirite(game.width/2, height/2,"player");
+  this.player = game.add.sprite(game.width/2, height/2,'player');
   this.player.anchor.setTo(0.5,0.5);
-
-game.physics.arcade.enable(this.player);
-
-this.player.body.gravity.y = 500;
-
-this.cursor = game.input.keyboard.createCursorKeys();
-
-
-
+  game.physics.arcade.enable(this.player);
+  this.player.body.gravity.y = 500;
+  this.cursor = game.input.keyboard.createCursorKeys();
 },
-	
-	
 	update: function(){
 		this.movePlayer();
-
 	},
 
 	movePlayer: function(){
@@ -44,13 +32,11 @@ this.cursor = game.input.keyboard.createCursorKeys();
 		}
 		if(this.cursor.up.isDown && this.player.body.touching.down){
 			this.player.body.velocity.y = -320;
-			
-
 		}
 		}
 	}
 };
-var game = new phaser.Game(500, 340, phaser.AUTO,'gameDiv');
+var game = new phaser.Game(500, 340, Phaser.AUTO,'gameDiv');
 
 game.state.add("main",mainState);
  game.state.start("main");
